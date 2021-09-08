22b8faa354e78860a213eb402ac9735fdd6831b9
David Heinebäck <du222aa@student.lnu.se>
2021-09-08 16:32:49 +0200

    add: console.log tests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import Tokenizer from "./Tokenizer.js"
+import WordGrammar from './WordGrammar.js'
+
 
 function tokenizer () {
-  const token = new Tokenizer('WORD_AND_DOT', 'Sträng m.ed tokens.')
-  token.getAllTokens()
+  const lexicalGrammar = new WordGrammar()
+  const token = new Tokenizer(lexicalGrammar, 'Sträng m.ed tokens.')
+  console.log(token.createTokens())
+  console.log(token.createAndSpecifyTokens())
+  console.log(`\n String contains ${token.countTokens()} valid tokens \n`)
 
   // token = new Tokenizer('ARITHMETIC', 'Sträng')
   // token.firstToken()
