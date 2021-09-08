775a2c7083f963bdd88a18dda59f03841f8d164e
David Heinebäck <du222aa@student.lnu.se>
2021-09-08 08:11:34 +0200

    add: simple get test to see if deconstruct of Tokenizer class works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import Tokenizer from "./Tokenizer.js"
 
 function tokenizer () {
-  const token = new Tokenizer()
+  const token = new Tokenizer('WORD_AND_DOT', 'Sträng med tokens.')
+  token.getName()
+
+  // token = new Tokenizer('ARITHMETIC', 'Sträng')
+  // token.firstToken()
+  // token.activeToken()
 }
+
+  // 'WORD_AND_DOT'
+  // 'ARITHMETIC'
 
 tokenizer()
