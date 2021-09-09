12da3e08f18de67e2ccb41880bac6fbfb1efaa46
David Heinebäck <du222aa@student.lnu.se>
2021-09-09 14:45:41 +0200

    add: create a new arithmetic tokenizer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import Tokenizer from "./Tokenizer.js"
 import WordGrammar from './WordGrammar.js'
+import ArithmeticGrammar from "./ArithmeticGrammar.js"
 
 
 function tokenizer () {
-  const lexicalGrammar = new WordGrammar()
-  const token = new Tokenizer(lexicalGrammar, 'Sträng m.ed tokens.')
+  const wordGrammar = new WordGrammar()
+  const arithmeticGrammar = new ArithmeticGrammar()
+
+  const token = new Tokenizer(wordGrammar, 'Sträng m.ed tokens.')
   console.log(token.createTokens())
   console.log(token.createAndSpecifyTokens())
   console.log(`\n String contains ${token.countTokens()} valid tokens \n`)
+  
+  const token2 = new Tokenizer(arithmeticGrammar, '11')
+  console.log(token2.createTokens())
+  
 
-  // token = new Tokenizer('ARITHMETIC', 'Sträng')
-  // token.firstToken()
-  // token.activeToken()
 }
 
-  // 'WORD_AND_DOT'
-  // 'ARITHMETIC'
 
 tokenizer()
