1ca3dc103bb9cf9bb15a7c0b62d6769c8443e72b
David Heinebäck <du222aa@student.lnu.se>
2021-09-12 12:03:08 +0200

    add: export modules to work with npm

--- src/index.js
+++ src/index.js
@@ -4,17 +4,11 @@ import ArithmeticGrammar from "./ArithmeticGrammar.js"
 import MaximalMunchGrammar from './MaximalMunchGrammar.js'
 import ExclamationGrammar from './ExclamationGrammar.js'
 
-function tokenizer() {
-  const wordGrammar = new WordGrammar()
-  const arithmeticGrammar = new ArithmeticGrammar()
-  const maximalMunchGrammar = new MaximalMunchGrammar()
-  const exclamationGrammar = new ExclamationGrammar()
-
-  const token = new Tokenizer(exclamationGrammar, '! + H!EJ')
-  token.setActiveTokenToNext()
-  
-  console.log(token.getActiveToken())
-  
+export default {
+  Grammar: { 
+    WordGrammar,
+    ArithmeticGrammar,
+    MaximalMunchGrammar,
+    ExclamationGrammar },
+  Tokenizer
 }
-
-tokenizer()
