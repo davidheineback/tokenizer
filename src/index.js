602da3064dd180624afb085d1a2a8231e1ebd999
David Heinebäck <du222aa@student.lnu.se>
2021-09-28 09:27:29 +0200

    refactor: fix export bug to npmjs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import Tokenizer from "./Tokenizer.js"
+import Grammar from './Grammar.js'
 import WordGrammar from './WordGrammar.js'
 import ArithmeticGrammar from "./ArithmeticGrammar.js"
 import MaximalMunchGrammar from './MaximalMunchGrammar.js'
 import ExclamationGrammar from './ExclamationGrammar.js'
 
-export default {
-  Grammar: { 
-    WordGrammar,
-    ArithmeticGrammar,
-    MaximalMunchGrammar,
-    ExclamationGrammar },
-  Tokenizer
+export default { 
+  Tokenizer,
+  Grammar,
+  WordGrammar,
+  ArithmeticGrammar,
+  MaximalMunchGrammar,
+  ExclamationGrammar
 }
